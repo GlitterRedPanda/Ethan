5c92d1fec2bb68fe9def1cc02f9d575de659def5
Lara Lai <GlitterRedPanda@gmail.com>
2021-07-04 21:45:42 -0700

    Adding files

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Lara Lai on 6/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Ethan the Farter")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/EthanApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthanApp.swift
+//  Shared
+//
+//  Created by Lara Lai on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct EthanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
